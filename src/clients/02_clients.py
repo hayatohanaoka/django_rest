f619d8615ad3080b85a9ea7b3a479b4fdac03051
hayatohanaoka <suzugamori.hh@gmail.com>
2024-04-25 00:42:37 +0900

    Change clients requst code normal and abnormal

--- src/clients/02_clients.py
+++ src/clients/02_clients.py
@@ -2,6 +2,9 @@ import requests
 
 URL = 'http://127.0.0.1:8000/api/datetime/'
 
+print('============正常系=============')
+
+# GET 正常系
 get_response = requests.get(URL, params={'timezone': 'US/Eastern'})
 print(get_response.status_code)
 print(get_response.headers)
@@ -27,3 +30,21 @@ put_response = requests.put(URL)
 print(put_response.status_code)
 print(put_response.headers)
 print(put_response.text)
+
+
+
+print('============異常系=============')
+
+# GET 異常系
+get_response = requests.get(URL, params={'timezone': 'US/E'})
+print(get_response.status_code)
+print(get_response.headers)
+print(get_response.text)
+
+print('=========================')
+
+# POST 異常系
+post_response = requests.post(URL, data={'timezone': 'US/E'})
+print(post_response.status_code)
+print(post_response.headers)
+print(post_response.text)
--- src/first_rest_api/views.py
+++ src/first_rest_api/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views import View
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from datetime import datetime, timezone
 import pytz
+from pytz.exceptions import UnknownTimeZoneError
 
 # Create your views here.
 class IndexView(View):
@@ -14,24 +16,27 @@ class IndexView(View):
 
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def country_datetime(req):
-    if req.method == 'POST':
-        if 'timezone' in req.data.keys():        
-            requested_timezone = req.data['timezone']
-            tz = pytz.timezone(requested_timezone)    # reqのタイムゾーン
-            utc_datetime = datetime.now(timezone.utc) # UTC
-            return Response(
-                {f'Datetime POST: {requested_timezone}': utc_datetime.astimezone(tz)}
-            )
-    elif req.method == 'PUT':
-        print('PUTメソッドで送信')
-    elif req.method == 'DELETE':
-        print('DELETEメソッドで送信')
-    else:
-        if 'timezone' in req.GET.keys():
-            requested_timezone = req.GET['timezone']
-            tz = pytz.timezone(requested_timezone)
-            utc_datetime = datetime.now(timezone.utc)
-            return Response({f'Datetime GET: {requested_timezone}': utc_datetime.astimezone(tz)})
-    return Response({'Datetime': datetime.now()})
+    try:
+        if req.method == 'POST':
+            if 'timezone' in req.data.keys():        
+                requested_timezone = req.data['timezone']
+                tz = pytz.timezone(requested_timezone)    # reqのタイムゾーン
+                utc_datetime = datetime.now(timezone.utc) # UTC
+                return Response(
+                    {f'Datetime POST: {requested_timezone}': utc_datetime.astimezone(tz)}
+                )
+        elif req.method == 'PUT':
+            print('PUTメソッドで送信')
+        elif req.method == 'DELETE':
+            print('DELETEメソッドで送信')
+        else:
+            if 'timezone' in req.GET.keys():
+                requested_timezone = req.GET['timezone']
+                tz = pytz.timezone(requested_timezone)
+                utc_datetime = datetime.now(timezone.utc)
+                return Response({f'Datetime GET: {requested_timezone}': utc_datetime.astimezone(tz)})
+        return Response({'Datetime': datetime.now()})
+    except UnknownTimeZoneError as e:
+        return Response({'Error request': 'Unknown timezone error'}, status=status.HTTP_400_BAD_REQUEST)
 
 index = IndexView.as_view()
