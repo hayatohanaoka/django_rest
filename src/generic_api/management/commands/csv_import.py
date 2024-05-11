064d7c5b2baf93d33494c1f61661d7111b623b3c
hayatohanaoka <suzugamori.hh@gmail.com>
2024-05-12 02:05:42 +0900

    Insert post and comment

--- src/generic_api/management/commands/csv_import.py
+++ src/generic_api/management/commands/csv_import.py
@@ -5,32 +5,33 @@ from typing import Any
 
 from django.core.management import BaseCommand
 from django.contrib import auth
-
+from ...models import Comment, Post
 
 class Command(BaseCommand):
+
     def handle(self, *args: Any, **options: Any) -> str | None:
         csv_dir_path = os.path.join(Path(__file__).resolve().parent, 'csvs')
 
-        comments_path = os.path.join(csv_dir_path, 'comments.csv')
-        posts_path = os.path.join(csv_dir_path, 'posts.csv')
-        users_path = os.path.join(csv_dir_path, 'users.csv')
-        
-        comments = self._read_csv_file(comments_path)
-        posts    = self._read_csv_file(posts_path)
-        users    = self._read_csv_file(users_path)
-        self._insert_user_data(auth.get_user_model(), users)
+        # user -> post -> comment の順で紐づくため、この順番でないとダメ
+        models = (auth.get_user_model(), Post, Comment)
+        files  = ('users.csv', 'posts.csv', 'comments.csv')
 
+        for file, model in zip(files, models):
+            path = os.path.join(csv_dir_path, file)
+            data = self._read_csv_file(path)
+            self._insert_data(model, data)
 
     def _read_csv_file(self, file_path):
         with open(file_path, 'r', encoding='utf-8', newline='') as f:
-            data = [row for row in csv.DictReader(f)]
+            data = [row_dict for row_dict in csv.DictReader(f)]
         f.close()
         return data
 
-    def _insert_user_data(self, user_model, user_data: dict):
+    def _insert_data(self, model, data: dict):
         insert_list = []
-        for row in user_data:
-            new = user_model(**row)
-            new.set_password(raw_password='12345678')
+        for row in data:
+            new = model(**row)
+            if model == auth.get_user_model():
+                new.set_password(raw_password='12345678')
             insert_list.append(new)
-        user_model.objects.bulk_create(insert_list)
+        model.objects.bulk_create(insert_list)
